d0a45bd5a55b6f93217f2b58ccff33dc4604f557
Александр Питасов <alexpit63@gmail.com>
2018-10-05 16:30:51 +0300

    Настроил все кнопки и элементы основного меню - исправил отображение при изменении размера окна, добавил экшены кнопок и элементов меню

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,99 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
 
 
+    public void leftAlignmentAction(ActionEvent actionEvent) {
+        System.out.println("Button1");
+    }
+
+    public void centerAlignmentAction(ActionEvent actionEvent) {
+        System.out.println("Button2");
+    }
+
+    public void rightAlignmentAction(ActionEvent actionEvent) {
+        System.out.println("Button3");
+    }
+
+    public void createFileAction(ActionEvent actionEvent) {
+        System.out.println("Creating a new file...");
+    }
+
+    public void openAction(ActionEvent actionEvent) {
+        System.out.println("Initial opening a file...");
+    }
+
+    public void saveAction(ActionEvent actionEvent) {
+        System.out.println("Saving the file...");
+    }
+
+    public void saveAsAction(ActionEvent actionEvent) {
+        System.out.println("Initial saving the file...");
+    }
+
+    public void exitAction(ActionEvent actionEvent) {
+        System.out.println("Shutdown...");
+    }
+
+    public void fontChooseAction(ActionEvent actionEvent) {
+
+    }
+
+    public void indentsAction(ActionEvent actionEvent) {
+
+    }
+
+    public void wordWrapAction(ActionEvent actionEvent) {
+
+    }
+
+    public void manualWinAction(ActionEvent actionEvent) {
+
+    }
+
+    public void aboutWinAction(ActionEvent actionEvent) {
+
+    }
+
+    public void printAction(ActionEvent actionEvent) {
+
+    }
+
+    public void cancelAction(ActionEvent actionEvent) {
+
+    }
+
+    public void cutAction(ActionEvent actionEvent) {
+
+    }
+
+    public void copyAction(ActionEvent actionEvent) {
+
+    }
+
+    public void pasteAction(ActionEvent actionEvent) {
+
+    }
+
+    public void deleteAction(ActionEvent actionEvent) {
+
+    }
+
+    public void findAction(ActionEvent actionEvent) {
+
+    }
+
+    public void replaceAction(ActionEvent actionEvent) {
+
+    }
+
+    public void highliteAction(ActionEvent actionEvent) {
+
+    }
+
+    public void insertDataAction(ActionEvent actionEvent) {
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,16 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    public Button leftButton = new Button("", new ImageView("sample/image/left.png"));
+    public Button centerButton;
+    public Button rightButton;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("JNotePad");
-        primaryStage.setScene(new Scene(root, 600, 500));
+        primaryStage.setScene(new Scene(root, 650, 500));
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
