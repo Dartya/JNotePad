40668e626089ea3696e12c80a571a9c4baeafe82
Alex <alexpit63@gmail.com>
2018-10-06 23:14:55 +0300

    добавил окно поиска текста

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,20 +1,42 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
 
+    public Button leftButton;
+    public Button centerButton;
+    public Button rightButton;
+    public TextArea textAreaOne = new TextArea();
+    public TextField searchTextField;
+    public Label findTextLabel;
+    public Button okTextFindButton ;
+    public Button cancelTextFindButton ;
+    public MenuItem findMenuItem;
 
     public void leftAlignmentAction(ActionEvent actionEvent) {
         System.out.println("Button1");
+        textAreaOne.setText("Button 1");
     }
 
     public void centerAlignmentAction(ActionEvent actionEvent) {
         System.out.println("Button2");
+        textAreaOne.setText("Button 2");
     }
 
     public void rightAlignmentAction(ActionEvent actionEvent) {
         System.out.println("Button3");
+
     }
 
     public void createFileAction(ActionEvent actionEvent) {
@@ -34,7 +56,7 @@ public class Controller {
     }
 
     public void exitAction(ActionEvent actionEvent) {
-        System.out.println("Shutdown...");
+
     }
 
     public void fontChooseAction(ActionEvent actionEvent) {
@@ -82,7 +104,21 @@ public class Controller {
     }
 
     public void findAction(ActionEvent actionEvent) {
+        try{
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("find_win.fxml"));
+            stage.setTitle("Найти");
+            stage.setMinHeight(200);
+            stage.setMinWidth(150);
+            stage.setResizable(false);
+            stage.setScene(new Scene(root));
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(((Node)actionEvent.getSource()).getScene().getWindow());    //указывается родительское окно
+            stage.show();
 
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     public void replaceAction(ActionEvent actionEvent) {
@@ -90,10 +126,18 @@ public class Controller {
     }
 
     public void highliteAction(ActionEvent actionEvent) {
-
+        textAreaOne.selectAll();
     }
 
     public void insertDataAction(ActionEvent actionEvent) {
+
+    }
+
+    public void textFindOkButtonAction(ActionEvent actionEvent) {
+
+    }
+
+    public void textFindCancelButtonAction(ActionEvent actionEvent) {
 
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,23 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    public Button leftButton = new Button("", new ImageView("sample/image/left.png"));
-    public Button centerButton;
-    public Button rightButton;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("JNotePad");
+        primaryStage.setMinHeight(300);
+        primaryStage.setMinWidth(610);
         primaryStage.setScene(new Scene(root, 650, 500));
         primaryStage.show();
-
     }
 
     public static void main(String[] args) {
